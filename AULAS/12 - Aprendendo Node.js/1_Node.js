b2f598ad23b625db4705f2bceb6fd7eb31976f47
Bruno Nishimura <by.nishimura@gmail.com>
2021-05-23 20:48:05 -0300

    chore: node WIP

--- AULAS/12 - Aprendendo Node.js/1_Node.js
+++ AULAS/12 - Aprendendo Node.js/1_Node.js
@@ -1,4 +1,38 @@
 //Aula 314-315
 
-/* dwd*/
+/*O que é Node?
+
+- Não é uma linguagem de programação;
+- Uma ferramenta construída sobre o motor JavaScript do Google Chrome para criar aplicações rápidas e escaláveis;
+- O problema que o Node resolveu: cada conexão de um cliente aloca um espaço na memória
+- Com Node: cada conexão é um evento executado na engine no Node;
+- Ou seja, o Node suporta muito mais conexões do que o PHP e Java em uma máquina igual, por exemplo;
+*/
+
+
+/*Instalando o Node
+- Node -v && npm -v; // Verificação se o Node e o npm estão instalados corretamente e as versões dos mesmos.
+*/
+
+/*Testando o Node
+- Podemos criar um arquivo JavaScript e executar com o node;
+- node test.js// Comando no terminal
+
+No arquivo test.js:
+  let msg = "Hello World";
+  console.log(mdg);  //Hello World
+*/
+
+/*O comando node
+- Podemos executar instruções de JavaScript rodando o comando node;;
+- Podemos também fazer operações matemáticas, utilizar funções do JavaScript e muito mais;
+
+Comando no terminal:
+node
+1+1
+console.log("teste"); //teste
+Math.pow(4,5);
+
+let msg = 'Bruno';
+console.log(msg); //Bruno
 */
